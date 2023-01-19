6d9d76a7bad27712d5ba7d81709296c1b8d1e585
Ade Balogun <abalogun316@gmail.com>
2023-01-19 10:29:10 -0500

    filter bad timestamps

--- src/routes/tasks/tasks_view.py
+++ src/routes/tasks/tasks_view.py
@@ -70,8 +70,8 @@ def view_task(logistics_id):
             "dt_range_end": time_range[dt_range_end_index]
         })
 
-        if timeslot.dt_range_start == datetime.min: continue
-        if timeslot.dt_range_end == datetime.min: continue
+        if timeslot.dt_range_start < datetime.fromtimestamp(0): continue
+        if timeslot.dt_range_end < datetime.fromtimestamp(0): continue
 
         timeslot_to_dict = timeslot.to_dict()
         timeslots_to_dict.append(timeslot_to_dict)
